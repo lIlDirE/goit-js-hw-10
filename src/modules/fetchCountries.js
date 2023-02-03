dff2b0c23ea0c91e8a45e657a14a04728f37fb21
dire <example@gmail.com>
2023-02-03 09:56:09 +0200

    limit 10  add

--- src/modules/fetchCountries.js
+++ src/modules/fetchCountries.js
@@ -1,7 +1,7 @@
 import { createCountryCard } from "./index.js"; 
 
 export function returnFetch(name){
-	return fetch(`https://restcountries.com/v3.1/name/${name}`)
+	return fetch(`https://restcountries.com/v3.1/name/${name}?_limit=10`)
 	.then((response) => response.json()
 	).then(createCountryCard).catch(error => console.log(error));
 }
